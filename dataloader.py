ed2a4060067cc27cff840b20f2f9f011e4337ecf
John Wu <jhnwu3@gmail.com>
2022-06-22 16:20:29 -0400

    $

--- dataloader.py
+++ dataloader.py
@@ -6,6 +6,8 @@ with h5py.File('CyTOFData.mat', 'r') as f:
     
 arrays = {}
 for k, v in f.items():
+    print(k)
+    print(v)
     arrays[k] = np.array(v)
 # data_dict = mat73.loadmat("CyTOFData.mat")
 # print(len(data_dict))
