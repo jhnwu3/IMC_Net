22bc8e628741a2a7cbf7c7d2b5b1b4765613c802
John Wu <jhnwu3@gmail.com>
2022-06-22 14:34:56 -0400

    $

--- dataloader.py
+++ dataloader.py
@@ -2,5 +2,5 @@ import mat73
 import h5py
 with h5py.File('CyTOFData.mat', 'r') as f:
     print(f.keys())
-data_dict = mat73.loadmat("CyTOFData.mat")
-print(len(data_dict))
+# data_dict = mat73.loadmat("CyTOFData.mat")
+# print(len(data_dict))
