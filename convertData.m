52cdae92cddcd871a93cfdb7bb49c034d8a4aca4
John Wu <jhnwu3@gmail.com>
2022-07-04 17:43:07 -0400

    hopefully works

--- convertData.m
+++ convertData.m
@@ -1,3 +1,15 @@
-FileData = load('CyTOFData.mat');
+%FileData = load('CyTOFData.mat');
 % csvwrite('CyTOFData.csv', FileData)
-save('FileData','-v6')
+%save('FileData','-v6')
+
+names = whos('-file','CyTOFData.mat');
+tiff = load('CyTOFData.mat',names(5).name);
+root_folder_name = 'png';
+for i = 1:height(tiff.Tiff_all)
+    mkdir([root_folder_name, sprintf('%d',i)])
+    for j = j:width(tiff.Tiff_all)
+        tiff_mat = cell2mat(tiff.Tiff_all(tiff.Tiff_all(i,j)));
+        imwrite(tiff_mat, fullfile(strcat(root_folder_name,sprintf('%d',i)),strcat(sprintf('%d',j),'.png')))
+    end
+
+end
